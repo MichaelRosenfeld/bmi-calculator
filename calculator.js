d571a812ffba0d083cf51b515d447df85e0a51f6
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-06-30 13:53:56 +0200

    Switched in app.get() from send to sendFile.

--- calculator.js
+++ calculator.js
@@ -1,11 +1,9 @@
-
-
 const express = require('express');
 const app = express();
 
 
 app.get("/", function(req, res) {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/index.html");
 })
 
 app.listen(3000, function() {
