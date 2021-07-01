c3444b4819857ee963e3fa3d7fead8497160e70f
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-07-01 09:19:23 +0200

    Added BMI Calculator

--- calculator.js
+++ calculator.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.listen(3000, function() {
+  console.log("Server spooled up at port 3000");
+});
+
+
+//Adder
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -10,14 +19,26 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-var num1 = Number(req.body.num1);
-var num2 = Number(req.body.num2);
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
 
-var result = num1 + num2;
+  var result = num1 + num2;
 
   res.send("The result is: " + result);
 });
 
-app.listen(3000, function() {
-  console.log("Server spooled up at port 3000");
+
+//BMI Calculator
+
+app.get("/bmiCalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator.html", function(req, res) {
+
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var bmi = Math.round(weight / (height/100 * height/100));
+
+  res.send("Your BMI is: " + bmi);
 });
